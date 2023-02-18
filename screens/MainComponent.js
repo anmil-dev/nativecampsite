7e050ff5bba2c559559e8dff8de774bd99bda676
Allison Miller <anmil.dev@gmail.com>
2023-02-18 08:25:58 -0500

    Navigation Part 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,20 +1,37 @@
-import { View, Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
-import { createStackNavigator } from '@react-navigation/stack';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+    headerTintColor: '#fff',
+    headerStyle: { backgroundColor: '#5637DD' }
+};
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{ title: 'Home' }}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
     return (
         <Stack.Navigator
-            initialRouteName = 'Directory'
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#5637DD'
-                },
-                headerTintColor: '#fff'
-            }}
+            initialRouteName='Directory'
+            screenOptions={screenOptions}
         >
             <Stack.Screen
                 name='Directory'
@@ -22,9 +39,9 @@ const DirectoryNavigator = () => {
                 options={{ title: 'Campsite Directory' }}
             />
             <Stack.Screen
-                name = 'CampsiteInfo'
-                component = { CampsiteInfoScreen }
-                options = {({route}) => ({
+                name='CampsiteInfo'
+                component={CampsiteInfoScreen}
+                options={({ route }) => ({
                     title: route.params.campsite.name
                 })}
             />
@@ -33,7 +50,6 @@ const DirectoryNavigator = () => {
 };
 
 const Main = () => {
-    
     return (
         <View
             style={{
@@ -42,7 +58,21 @@ const Main = () => {
                     Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
             }}
         >
-            <DirectoryNavigator />
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{ backgroundColor: '#CEC8FF' }}
+            >
+                <Drawer.Screen
+                    name='Home'
+                    component={HomeNavigator}
+                    options={{ title: 'Home' }}
+                />
+                <Drawer.Screen
+                    name='Directory'
+                    component={DirectoryNavigator}
+                    options={{ title: 'Directory' }}
+                />
+            </Drawer.Navigator>
         </View>
     );
 };
